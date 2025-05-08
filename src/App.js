e1bc5412b0d3d5b5d92c923ab281d84cf82d15fb
UshanBc <ushanabey@gmail.com>
2025-05-08 14:45:50 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hi....,i am ushan.
         </p>
         <a
           className="App-link"
